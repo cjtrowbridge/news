d4fbcc3a455d45f4e3be4c0030e2eb785f14092e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-30 09:23:15 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="icon" href="">
+
+  <title>Damage Report Global</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
+  
+  <link rel="stylesheet" href="/css/style.css">
+
+  </head>
+
+<body>
+
+  <nav class="navbar navbar-fixed-top navbar-dark bg-primary container">
+    <a class="navbar-brand" href="https://astria.tech">Astria</a>
+    <ul class="nav navbar-nav">
+          </ul>
+      </nav>
+
+  <div class="container" id="bodyContainer">
+
+    	
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+          <h1>Damage Report Global</h1>
+      </div>
+	  </div>
+	  <div class="row">
+			<div class="col-xs-12 col-md-5">
+          <img src="">
+      </div>
+			<div class="col-xs-12 col-md-7">
+          <img src="">
+      </div>
+	  </div>
+	
+	
+  </div><!-- /.container -->
+
+</body>
+</html>
