61466052f3137355784ba0533829f2b6b2ca77de
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-30 21:43:21 -0800

    Update damagereport.js

--- js/damagereport.js
+++ js/damagereport.js
@@ -5,8 +5,7 @@ $(".feedContainer").each(function(){
 
   $.get(feed, function(data){
     
-    var $channel      = $(data).find('channel');
-    var $channelTitle = $($channel).find('title:first-of-type')[0];
+    var $channelTitle      = $(data).find('title:first-of-type').text();
     console.log($channelTitle);
     
     $(data).find('item').each(function(){
