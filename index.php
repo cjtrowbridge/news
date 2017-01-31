d6a0d30498b042108c4a2abfe1dd98c2fe3c5d90
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-30 22:05:31 -0800

    Update index.php

--- index.php
+++ index.php
@@ -64,10 +64,9 @@ function FetchFeed($url){
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=269096993490945";
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1203074133061923";
     fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  </script>
+  }(document, 'script', 'facebook-jssdk'));</script>
  
   <nav class="navbar navbar-fixed-top navbar-dark bg-primary container">
     <a class="navbar-brand" href="https://damagereport.cjtrowbridge.com">Damage Report</a>
@@ -75,7 +74,7 @@ function FetchFeed($url){
       
     </ul>
     <div class="facebookBox">
-      <div class="fb-like" data-href="//damagereport.cjtrowbridge.com/" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
+      <div class="fb-like" data-href="https://damagereport.cjtrowbridge.com/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
     </div>
   </nav>
 
