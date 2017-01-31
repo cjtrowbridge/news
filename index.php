f3df346946ae6aa1f8c632b37046b7fd1e8f8234
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-30 20:22:58 -0800

    Update index.php

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ function FetchFeed($url){
         $player+='><source src="'+$url+'" type="audio/mp3"></audio>';
         $(podcastContainer).find(".player").html($player);
         $(podcastContainer).find("audio").on('ended', function(){
-          $(this).remove();
+          $('.podcastContainer:first-of-type').remove();
           $('audio:first-of-type')[0].play();
        });
       });
