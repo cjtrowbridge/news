f7250c27cdba232a8b86c83a402f555b1165dabc
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-30 20:31:23 -0800

    Update index.php

--- index.php
+++ index.php
@@ -41,12 +41,23 @@ function FetchFeed($url){
 </head>
 
 <body>
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=269096993490945";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
 
   <nav class="navbar navbar-fixed-top navbar-dark bg-primary container">
     <a class="navbar-brand" href="https://damagereport.cjtrowbridge.com">Damage Report</a>
     <ul class="nav navbar-nav">
       
     </ul>
+    <div class="facebookBox">
+      <div class="fb-like" data-href="http://damagereport.cjtrowbridge.com/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
+    </div>
   </nav>
 
   <div class="container" id="bodyContainer">
