c4935513baee1a2d76ccbf4d596cd989e42ee6fd
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-30 20:55:25 -0800

    Update index.php

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ function FetchFeed($url){
       <div class="row">
         <div class="col-xs-12 col-md-5 podcastColumn">
         <img src="/img/damage-report.jpg" class="picard">
-        <h4>Quality Sources</h4>
+        <h4>Choose Quality Sources</h4>
         <p>These high quality news sources do not include any commentary, just news.</p>
         <div class="podcastContainer" data-feed="/?feed=npr">
           <img src="https://media.npr.org/images/podcasts/2013/primary/hourly_news_summary-c464279737c989a5fbf3049bc229152af3c36b9d.png?s=1400">
