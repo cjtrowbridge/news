ca07fbaf55d477e7c21dee2af948d015ba05c6a5
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-30 22:02:22 -0800

    Update damagereport.js

--- js/damagereport.js
+++ js/damagereport.js
@@ -1,3 +1,40 @@
+$(".podcastContainer").each(function(){
+  
+  var podcastContainer = $(this);
+  var feed = $(podcastContainer).data('feed');
+
+  $.get(feed, function(data){
+    
+    var $url = $(data).find("enclosure:first-of-type").attr('url');
+    
+    var $player='<audio controls preload';
+    if(feed=='/?feed=npr-podcast'){
+       $player+=' autoplay';
+    }
+    $player+='><source src="'+$url+'" type="audio/mp3"></audio>';
+    
+    $(podcastContainer).find(".player").html($player);
+    
+    if(feed=='/?feed=npr'){
+      $('audio:first-of-type')[0].play();
+    }
+    
+    $(podcastContainer).find("audio").on('ended', function(){
+     
+     var $dead = $('.podcastContainer:first-of-type');
+     $.when($dead.fadeOut(400))
+     .done(function() {
+      $dead.remove();
+      $('audio:first-of-type')[0].play();
+     });
+
+   });
+   
+  });
+
+});
+
+
 $(".feedContainer").each(function(){
   
   var feedContainer = $(this);
@@ -26,43 +63,6 @@ $(".feedContainer").each(function(){
        $('#feedContainer').append('<div class="card newsStory" data-pubdate="'+$pubDate+'"><div class="card-block"><h4 class="card-title">'+$title+'</h4><i>'+$channelTitle+'</i><p class="card-text">'+$description+'</p></div></div>');
      
     });
-   
-  });
-
-});
-
-
-$(".podcastContainer").each(function(){
-  
-  var podcastContainer = $(this);
-  var feed = $(podcastContainer).data('feed');
-
-  $.get(feed, function(data){
-    
-    var $url = $(data).find("enclosure:first-of-type").attr('url');
-    
-    var $player='<audio controls preload';
-    if(feed=='/?feed=npr'){
-       $player+=' autoplay';
-    }
-    $player+='><source src="'+$url+'" type="audio/mp3"></audio>';
-    
-    $(podcastContainer).find(".player").html($player);
-    
-    if(feed=='/?feed=npr'){
-      $('audio:first-of-type')[0].play();
-    }
-    
-    $(podcastContainer).find("audio").on('ended', function(){
-     
-     var $dead = $('.podcastContainer:first-of-type');
-     $.when($dead.fadeOut(400))
-     .done(function() {
-      $dead.remove();
-      $('audio:first-of-type')[0].play();
-     });
-
-   });
    
   });
 
