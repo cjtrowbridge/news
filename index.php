e4867c203ba9e13fb59ea4fcddadb9ca26dce42f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-30 20:35:33 -0800

    Update index.php

--- index.php
+++ index.php
@@ -92,34 +92,6 @@ function FetchFeed($url){
   
   </div><!-- /.container -->
     
-  <script>
-    $(".podcastContainer").each(function(){
-      var podcastContainer = $(this);
-      var feed = $(podcastContainer).data('feed');
-      
-      $.get(feed, function(data){
-        var $url = $(data).find("enclosure:first-of-type").attr('url');
-        var $player='<audio controls preload';
-        if(feed=='/?feed=npr'){
-           $player+=' autoplay';
-        }
-        $player+='><source src="'+$url+'" type="audio/mp3"></audio>';
-        $(podcastContainer).find(".player").html($player);
-        $(podcastContainer).find("audio").on('ended', function(){
-         var $dead = $('.podcastContainer:first-of-type');
-         $.when($dead.fadeOut(400))
-         .done(function() {
-          $dead.remove();
-          $('audio:first-of-type')[0].play();
-         });
-          
-       });
-      });
-      
-    });
-    
-    
-    
-  </script>
+  <script src="/js/damagereport.js"></script>
 </body>
 </html>
